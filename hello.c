3178b6c82819be6c0d71b9937a88f25894cc4906
jahudgens <42652413+jahudgens@users.noreply.github.com>
2018-08-23 13:13:13 -0500

    updated names

--- hello.c
+++ hello.c
@@ -10,7 +10,7 @@
 
 int main(int argc, char **argv) {
 
-  printf("Nathan Sabin, Computer Science!\n");
+  printf("Nathan Sabin, Computer Science, Jason Hudgens, Mechanical Engineer!\n");
 
   return 0;
 }
