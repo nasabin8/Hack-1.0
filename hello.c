7cbfbac0c2f8b104a88f9697d3f6ca8481a3690b
nasabin8 <42580588+nasabin8@users.noreply.github.com>
2018-08-23 13:04:23 -0500

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Nathan Sabin
+ * Date: 2018/23/08
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Nathan Sabin, Computer Science!\n");
+
+  return 0;
+}
